6cd1210227df84f48e33837f047f0e9d84b13ad5
qrsor <qrsor85@o2.pl>
2013-12-24 11:18:03 +0100

    Learned groovy collections

---
+++ learn.groovy
@@ -0,0 +1,51 @@
+println "Hello World!"
+
+def x = 1
+println x
+
+x = new java.util.Date()
+println x
+
+x = -3.1499392
+println x
+
+x = false
+println x
+
+x = "Groovy!"
+println x
+
+def technologies = []
+
+technologies.add('Grails!')
+
+technologies << 'Groovy'
+
+technologies.addAll(['Griffon','Gradle'])
+
+technologies.remove('Griffon')
+
+technologies = technologies - 'Grails!'
+
+technologies.each {println "Technology: $it"}
+
+technologies.eachWithIndex {it, i -> println "$i: $it"}
+
+contained = technologies.contains('Groovy')
+
+contained = 'Gradle' in technologies
+
+technologies.containsAll(['Gradle','Groovy'])
+
+//technologies.sort()
+sortedTechnologies = technologies.sort(false)
+
+Collections.replaceAll(technologies,'Gradle','gradle')
+
+technologies << 'test'
+
+Collections.shuffle(technologies, new Random())
+
+println technologies
+
+/* Maps--------------------------------------*/
